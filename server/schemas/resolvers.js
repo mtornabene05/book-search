1dcce6759222ed07c977bd9651ef9cd87d65d4bf
Maria <mtornabene05@gmail.com>
2022-05-01 15:38:45 -0400

    resolvers in place

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id });
+
+                return userData;
+            }
+
+            throw new AuthenticationError('you are not logged in');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if(!user) {
+                throw new AuthenticationError('incorrect credentials');
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if(!correctPassword) {
+                throw new AuthenticationError('incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('you need to login first');
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: bookId } },
+                    { new: true }
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('you need to login first');
+        }
+    }
+};
+
+module.exports = resolvers;
